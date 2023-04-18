095bea8870e1eb22faf2deaef5625b7b39bf6878
Francisco <franciscomaia097@gmail.com>
2023-04-18 16:13:17 +0100

    Class: Postion

---
+++ src/aplication/Main.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import boardGame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos.toString());
+    }
+}
